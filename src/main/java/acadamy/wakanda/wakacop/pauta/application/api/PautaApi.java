2895c764238b607df8731c6393e0b6991e776728
Ronan Lima <ronanlimasilva@hotmail.com>
2024-09-17 22:01:50 -0300

    fix api response e controller

--- src/main/java/acadamy/wakanda/wakacop/pauta/application/api/PautaApi.java
+++ src/main/java/acadamy/wakanda/wakacop/pauta/application/api/PautaApi.java
@@ -1,10 +1,12 @@
 package acadamy.wakanda.wakacop.pauta.application.api;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pauta")
 public interface PautaApi {
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public PautaCadastradaResponse cadastraPauta(@RequestBody NovaPautaRequest novaPauta);
 }
